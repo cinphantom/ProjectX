4dab4a1ccc08e5efcf7c80e13a9125ff89d2f550
cinphantom <cinphantom@me.com>
2019-09-26 22:31:48 +0800

    9/26 pm 22:31

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,6 +2,9 @@
 class Login extends CI_Controller
 {
     public function index(){
+        $this->load->helper("form");
+
+
         if($this->input->method()=='post'){
             //validate the login post form
             $this->load->library('form_validation');
@@ -23,10 +26,26 @@ class Login extends CI_Controller
             //validate
             if($this->form_validation->run()){
 
-            }
+//                $user = $this->db->get_where('user',['username'=>$this->input->post('username')])->row_array();
+//                if(!$user){
+//                    echo();
+//                }
+                $html=<<<A
+                    <script
+                            type="text/javascript">
+                            $('.mask,.dialog').show();
+                            $('.dialog .dialog-hd p').html('Something goes wrong!');
+                            $('.dialog .dialog-bd p').html('没有找到该用户，请确认用户名');
+                        }
+                    </script>
+A;
 
+            }else{
+                $html="";
+            }
         }
+        $data=array('html'=>"haglkjas");
         $this->load->helper('url');
-        $this->load->view('login');
+        $this->load->view('login', $data);
     }
 }
--- application/views/login.php
+++ application/views/login.php
@@ -20,49 +20,71 @@
 <script src="<?=base_url().'resource/javascript/jquery.js' ?>"></script>
 <script src="<?=base_url().'resource/javascript/public.js'?>"></script>
 <script src="<?=base_url().'resource/javascript/plug-ins/customScrollbar.min.js'?>"></script>
-<!--<script src="--><?//=base_url().'resource/javascript/pages/login.js'?><!--"></script>-->
 </head>
+<?php echo $html?>
 <body class="login-page">
 	<section class="login-contain">
 		<header>
-			<h1>管理系统</h1>
+			<h1>试制信息化管理系统</h1>
 			<p>management system</p>
 		</header>
 		<div class="form-content">
-			<ul>
-				<li>
-					<div class="form-group">
-						<label class="control-label">账号：</label>
-						<input type="text" placeholder="您的账号..." class="form-control form-underlined" id="username"/>
-					</div>
-                    <div>
-                        <?=form_error('username')?>
-                    </div>
-				</li>
-				<li>
-					<div class="form-group">
-						<label class="control-label">密码：</label>
-						<input type="password" placeholder="您的密码..." class="form-control form-underlined" id="password"/>
-					</div>
-                    <div>
-                        <?=form_error('password')?>
-                    </div>
-				</li>
-				<li>
-					<label class="check-box">
-						<input type="checkbox" name="remember"/>
-						<span>记住账号密码</span>
-					</label>
-				</li>
-				<li>
-					<button class="btn btn-lg btn-block" id="entry">立即登录</button>
-				</li>
-				<li>
-					<p class="btm-info">©Copyright 2019  <a class="btm-info" href="#" target="_blank" title="DeathGhost">  dyk Prototype Team</a></p>
-					<address class="btm-info">JiangSu YanCheng</address>
-				</li>
-			</ul>
+            <?=form_open('','')?>
+                <ul>
+                    <li>
+
+                        <div class="form-group">
+                            <label class="control-label">账号：</label>
+                            <input type="text" placeholder="您的账号..." class="form-control form-underlined" id="username"/>
+                        </div>
+                        <div class="error-message">
+                            <?=form_error('username')?>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="form-group">
+                            <label class="control-label">密码：</label>
+                            <input type="password" placeholder="您的密码..." class="form-control form-underlined" id="password"/>
+                        </div>
+                        <div class="error-message">
+                            <?=form_error('password')?>
+                        </div>
+                    </li>
+                    <li>
+                        <label class="check-box">
+                            <input type="checkbox" name="remember"/>
+                            <span>记住账号密码</span>
+                        </label>
+                    </li>
+                    <li>
+                        <button class="btn btn-lg btn-block" id="entry">立即登录</button>
+                    </li>
+                    <li>
+                        <p class="btm-info">©Copyright 2019  <a class="btm-info" href="#" target="_blank" title="DeathGhost">  dyk Prototype Team</a></p>
+                        <address class="btm-info">JiangSu YanCheng</address>
+                    </li>
+                </ul>
+            </form>
 		</div>
 	</section>
+
+    <div class="mask"></div>
+    <div class="dialog">
+        <div class="dialog-hd">
+            <strong class="lt-title"><p>标题</p>></strong>
+            <a class="rt-operate icon-remove JclosePanel" title="关闭"></a>
+        </div>
+        <div class="dialog-bd">
+            <!--start::-->
+            <p>这里是基础弹窗,可以定义文本信息，HTML信息这里是基础弹窗,可以定义文本信息，HTML信息。</p>
+            <!--end::-->
+        </div>
+        <div class="dialog-ft">
+            <button class="btn btn-info JyesBtn">确认</button>
+            <button class="btn btn-secondary JnoBtn">关闭</button>
+        </div>
+    </div>
+
+
 </body>
 </html>
